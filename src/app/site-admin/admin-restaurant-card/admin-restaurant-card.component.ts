9099181fc3bcb4fb957ef8a8c45dc394a9aac592
ayush <saxenaa231112@gmail.com>
2023-10-08 23:02:08 +0530

    admin

--- src/app/site-admin/admin-restaurant-card/admin-restaurant-card.component.ts
+++ src/app/site-admin/admin-restaurant-card/admin-restaurant-card.component.ts
@@ -1,4 +1,7 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AdminService } from 'src/app/services/admin.service';
 
 @Component({
   selector: 'app-admin-restaurant-card',
@@ -7,7 +10,32 @@ import { Component, Input } from '@angular/core';
 })
 export class AdminRestaurantCardComponent {
   @Input() restaurant: any;
+  constructor(
+    private adminService: AdminService,
+    private toastr: ToastrService,
+    private router: Router
+  ) {}
   deleteRestaurant() {
-    console.log(this.restaurant);
+    console.log(this.restaurant.id);
+    this.adminService.deleteUser(this.restaurant.id).subscribe(
+      (response) => {
+        this.toastr.success('User deleted Successfully!', '', {
+          positionClass: 'toast-bottom-right',
+        });
+        this.reloadComponent();
+      },
+      (error) => {
+        console.error(error);
+        this.toastr.error('Deletion Failed!', '', {
+          positionClass: 'toast-bottom-right',
+        });
+      }
+    );
+  }
+  reloadComponent() {
+    let currentUrl = this.router.url;
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+    this.router.onSameUrlNavigation = 'reload';
+    this.router.navigate([currentUrl]);
   }
 }
